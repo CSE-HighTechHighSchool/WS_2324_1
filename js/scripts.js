4d6c8f06b901f2b5734701e6b400697a60c85579
A-Patel07 <ayanp2007@gmail.com>
2023-10-20 13:22:38 -0400

    Implemented weather.gov API for live temperature

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const apiEndpoint = "https://api.weather.gov/points/40.3291,-74.1240"; // Lincroft's coordinates
+const accessToken = "bLixBffiQkvdoZWKWCpvCZGraiuwMWbC"; // Access Token
+const temperatureElement = document.getElementById("temperature");
+console.log(temperatureElement);
+const headers = {
+  "Authorization": `Bearer ${accessToken}`
+};
+fetch(apiEndpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    fetch(data.properties.forecast)
+    .then(res => res.json())
+    .then(forecast => {
+      const temperature = forecast.properties.periods[0].temperature;
+      temperatureElement.textContent = `Temperature in Lincroft: ${temperature} °F`;
+    })
+    .catch(error => {
+      console.log(`Error: ${error}`);
+    })
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
